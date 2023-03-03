e42f4924bdaae081f213fab0e8378c4d3a233728
Tony Dandino <tdandino1005@gmail.com>
2023-03-02 20:54:02 -0600

    Pass text prop from parent to child

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-
-export default function Header() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-    }
+export default function Header(
+  // Props are passed in as an object. We can destructure the object to get the properties we want.
+  { text }
+) {
+  return <h1 className="text-3xl font-bold underline">{text}</h1>;
+}
