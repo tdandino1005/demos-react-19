becbbcae88cb6b76246fcf87c8ad211c06ff8f2f
Tony Dandino <tdandino1005@gmail.com>
2023-03-02 21:32:16 -0600

    Used fragment tags in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import CatCard from "./components/CatCard";
 import Header from "./components/Header";
+import catsData from "./data/cats";
 
 function App() {
-  return <Header text="Hello World" />;
+  return (
+    <>
+      <Header text="Hello cat" />
+      <CatCard cat={catsData[0]} />
+    </>
+  );
 }
-
 export default App;
